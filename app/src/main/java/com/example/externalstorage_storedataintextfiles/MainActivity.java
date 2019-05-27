eccc33b76b14c48b7e76dd7ebd4016f06bd5fc26
Aman Sharma <amansharma.11@hotmail.com>
2019-05-27 19:03:44 +0530

    commit

--- app/src/main/java/com/example/externalstorage_storedataintextfiles/MainActivity.java
+++ app/src/main/java/com/example/externalstorage_storedataintextfiles/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.externalstorage_storedataintextfiles;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +18,8 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final  int REQUEST_PERMISSION_WRITE = 1001;
+    private  boolean permissionGranted;
     private TextView textView;
     public static final String FILE_NAME = "my_file_external_storage";
 
@@ -25,15 +33,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private File getFile(){
+        return new File(Environment.getExternalStorageDirectory(),FILE_NAME);
+    }
+
+
     public void createBtn(View view) {
+
+        if (!permissionGranted)
+        {
+            checkPermissions();
+            return;
+        }
+
         String string = getString(R.string.text_lorem_ipsum);
 
         FileOutputStream fileOutputStream = null;
 
-        File file = new File(FILE_NAME);
+        File file = getFile();
 
         try {
-            fileOutputStream = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fileOutputStream = new FileOutputStream(file);
             fileOutputStream.write(string.getBytes());
             Toast.makeText(this,"File Written :"+FILE_NAME,Toast.LENGTH_SHORT).show();
         }
@@ -49,14 +69,74 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
     public void deleteBtn(View view) {
-        File file = new File(getFilesDir(),FILE_NAME) ;
+        if (!permissionGranted)
+        {
+            checkPermissions();
+            return;
+        }
+
+        File file = getFile();
         if(file.exists()){
-            deleteFile(FILE_NAME);
+            file.delete();
             Toast.makeText(this,"File Deleted",Toast.LENGTH_SHORT).show();
         }
         else{
             Toast.makeText(this,"File doesn't exist",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        return (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state));
+    }
+
+    // Initiate request for permissions.
+    private boolean checkPermissions() {
+
+        if (!isExternalStorageReadable() || !isExternalStorageWritable()) {
+            Toast.makeText(this, "This app only works on devices with usable external storage",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION_WRITE);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Handle permissions result
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_WRITE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    permissionGranted = true;
+                    Toast.makeText(this, "External storage permission granted",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "You must grant permission!", Toast.LENGTH_SHORT).show();
+                }
+                break;
         }
     }
 }
